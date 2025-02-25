5e1ca4e9a046906bfc721caa37f2cf71b9eeb000
Thilo Wendt <business@thilo-wendt.de>
2025-02-25 09:04:22 +0100

    Change main.py to generate questions for hunting exam

--- main.py
+++ main.py
@@ -1,51 +1,41 @@
+import json
+import pathlib
 import sys
+
+import genanki
 from dotenv import load_dotenv
-from unstract.llmwhisperer.client import LLMWhispererClient, LLMWhispererClientException
-from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from datetime import datetime
-from pydantic import BaseModel, Field
 from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate,
+                               SystemMessagePromptTemplate)
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from unstract.llmwhisperer.client_v2 import LLMWhispererClientV2
+
+ANKI_FILE = "anki/jaegerpruefung_tiere.apkg"
+ANKI_DECK_NAME = "Jägerprüfung::Tiere"
+ANKI_DECK_DESCRIPTION = (
+    "Fragen Stand Juli 2022 Fachgebiet 1: "
+    '"Dem Jagdrecht unterliegende und andere frei lebende Tiere" '
+    "für die Jägerprüfung Niedersachsen"
+)
+JSON_FILE = "json/tiere.json"
+INPUT_FILE = "assets/tiere.pdf"
+LAST_PAGE = 52
 
 
-class PersonalDetails(BaseModel):
-    name: str = Field(description="Name of the individual")
-    ssn: str = Field(description="Social Security Number of the individual")
-    dob: datetime = Field(description="Date of birth of the individual")
-    citizenship: str = Field(description="Citizenship of the individual")
+class Answer(BaseModel):
+    text: str = Field(description="Answer text without enumeration")
+    correct: bool = Field(description="True if the answer is correct false if not")
 
 
-class ExtraDetails(BaseModel):
-    type_of_credit: str = Field(description="Type of credit")
-    marital_status: str = Field(description="Marital status")
-    cell_phone: str = Field(description="Cell phone number")
+class Question(BaseModel):
+    question: str = Field(description="Text of the question")
+    number: int = Field(description="Number of the question")
+    answers: list[Answer] = Field(description="List of the possible answers")
 
 
-class CurrentAddress(BaseModel):
-    street: str = Field(description="Street address")
-    city: str = Field(description="City")
-    state: str = Field(description="State")
-    zip_code: str = Field(description="Zip code")
-    residing_in_addr_since_years: int = Field(description="Number of years residing in the address")
-    residing_in_addr_since_months: int = Field(description="Number of months residing in the address")
-    own_house: bool = Field(description="Whether the individual owns the house or not")
-    rented_house: bool = Field(description="Whether the individual rents the house or not")
-    rent: float = Field(description="Rent amount")
-    mailing_address_different: bool = Field(description="Whether the mailing address is different from the current "
-                                                        "address or not")
-
-
-class EmploymentDetails(BaseModel):
-    business_owner_or_self_employed: bool = Field(description="Whether the individual is a business owner or "
-                                                              "self-employed")
-    ownership_of_25_pct_or_more: bool = Field(description="Whether the individual owns 25% or more of a business")
-
-
-class Form1003(BaseModel):
-    personal_details: PersonalDetails = Field(description="Personal details of the individual")
-    extra_details: ExtraDetails = Field(description="Extra details of the individual")
-    current_address: CurrentAddress = Field(description="Current address of the individual")
-    employment_details: EmploymentDetails = Field(description="Employment details of the individual")
+class HuntingExam(BaseModel):
+    questions: list[Question] = Field(description="List of questions in the catalog")
 
 
 def error_exit(error_message):
@@ -53,46 +43,126 @@ def error_exit(error_message):
     sys.exit(1)
 
 
-def process_1003_information(extracted_text):
-    preamble = ("What you are seeing is a filled out 1003 loan application form. Your job is to extract the "
-                "information from it accurately.")
-    postamble = "Do not include any explanation in the reply. Only include the extracted information in the reply."
+def process_hunting_exam(extracted_text):
+    preamble = (
+        "What you are seeing are questions from an exam for the hunting license in Lower Saxony, Germany. "
+        "Your job is to extract only the questions without chapter headings to the given format."
+        "An [X] preceding the answer means that the answer is correct."
+    )
+    postamble = (
+        "Do not include any explanation in the reply. "
+        "Only include the extracted information in the reply. "
+        "Only output the plain JSON without markdown formatting instructions like ```json"
+    )
     system_template = "{preamble}"
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
     human_template = "{format_instructions}\n\n{extracted_text}\n\n{postamble}"
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-    parser = PydanticOutputParser(pydantic_object=Form1003)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-    request = chat_prompt.format_prompt(preamble=preamble,
-                                        format_instructions=parser.get_format_instructions(),
-                                        extracted_text=extracted_text,
-                                        postamble=postamble).to_messages()
+    parser = PydanticOutputParser(pydantic_object=HuntingExam)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+    request = chat_prompt.format_prompt(
+        preamble=preamble,
+        format_instructions=parser.get_format_instructions(),
+        extracted_text=extracted_text,
+        postamble=postamble,
+    ).to_messages()
     chat = ChatOpenAI()
     response = chat(request, temperature=0.0)
-    print(f"Response from LLM:\n{response.content}")
     return response.content
 
 
 def extract_text_from_pdf(file_path, pages_list=None):
-    llmw = LLMWhispererClient()
-    try:
-        result = llmw.whisper(file_path=file_path, pages_to_extract=pages_list)
-        extracted_text = result["extracted_text"]
-        return extracted_text
-    except LLMWhispererClientException as e:
-        error_exit(e)
+    llmw = LLMWhispererClientV2()
+    result = llmw.whisper(
+        file_path=file_path,
+        pages_to_extract=pages_list,
+        wait_for_completion=True,
+        output_mode="text",
+        page_seperator="",
+    )
+    return result
 
 
-def process_1003_pdf(file_path, pages_list=None):
+def process_pdf(file_path, pages_list) -> HuntingExam:
     extracted_text = extract_text_from_pdf(file_path, pages_list)
-    print(extracted_text)
-    response = process_1003_information(extracted_text)
+    response = process_hunting_exam(extracted_text)
+    return HuntingExam(**json.loads(response))
+
+
+def generate_flashcards(data: HuntingExam):
+    template_dir = "templates/"
+    with open(f"{template_dir}afmt.html", encoding="utf-8") as file:
+        afmt = file.read()
+    with open(f"{template_dir}qfmt.html", encoding="utf-8") as file:
+        qfmt = file.read()
+    with open(f"{template_dir}style.css", encoding="utf-8") as file:
+        style = file.read()
+    MODEL_ID = 1566095810
+    DECK_ID = 1996064686
+    model = genanki.Model(
+        MODEL_ID,
+        "Mulitple Choice",
+        fields=[
+            {"name": "Question"},
+            {"name": "Title"},
+            {"name": "QType (0=kprim,1=mc,2=sc)"},
+            {"name": "Q_1"},
+            {"name": "Q_2"},
+            {"name": "Q_3"},
+            {"name": "Q_4"},
+            {"name": "Q_5"},
+            {"name": "Q_6"},
+            {"name": "Answers"},
+            {"name": "Sources"},
+            {"name": "Extra 1"},
+        ],
+        templates=[{"name": "AllInOne", "qfmt": f"{qfmt}", "afmt": f"{afmt}"}],
+        css=f"{style}",
+    )
+    my_deck = genanki.Deck(
+        DECK_ID,
+        ANKI_DECK_NAME,
+        description=ANKI_DECK_DESCRIPTION,
+    )
+    for q in data.questions:
+        answers = []
+        correct = ""
+        for a in q.answers:
+            answers.append(a.text)
+            if a.correct:
+                correct += "1 "
+            else:
+                correct += "0 "
+        while len(answers) < 6:
+            answers.append("")
+        answers.append(correct.strip())
+        fields = [f"{q.question}", f"{q.number}", "1"]
+        fields.extend(answers)
+        fields.extend(["", ""])
+        my_deck.add_note(
+            genanki.Note(
+                model=model,
+                fields=fields,
+            )
+        )
+    pathlib.Path(ANKI_FILE).parent.mkdir(parents=True, exist_ok=True)
+    genanki.Package(my_deck).write_to_file(ANKI_FILE)
 
 
 def main():
-    load_dotenv()
-    process_1003_pdf("assets/docs/1003-sample.pdf", "1")
+    load_dotenv("./keys.env")
+    exam = process_pdf(INPUT_FILE, 1)
+    try:
+        for i in range(2, LAST_PAGE + 1):
+            exam.questions.extend(process_pdf(INPUT_FILE, f"{i}").questions)
+    finally:
+        pathlib.Path(JSON_FILE).parent.mkdir(parents=True, exist_ok=True)
+        with open(JSON_FILE, "w", encoding="utf-8") as file:
+            json.dump(exam.model_dump(), file, indent=2, ensure_ascii=False)
+    generate_flashcards(exam)
 
 
 if __name__ == "__main__":
